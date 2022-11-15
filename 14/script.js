e70e3b699c52ea020bf843dc26fe3a308ec263f3
Toni <tonifontm@gmail.com>
2022-11-15 14:13:07 +0100

    Terminado, ya se lo basico de JQuery

--- 14/script.js
+++ 14/script.js
@@ -1,6 +1,51 @@
 $(document).ready(function(){
+    $("#jquerySelectorB1").mousedown(function(){
+        $("#selector p").css("visibility","hidden");
+    });
+
+    $("#jquerySelectorB2").mousedown(function(){
+        $("#selector #test").css("visibility","hidden");
+    });
+
+    $("#jquerySelectorB3").mousedown(function(){
+        $("#selector .despedida").css("visibility","hidden");
+    });
+
+    $("#jquerySelectorB4").mousedown(function(){
+        $("#selector *").css("visibility","hidden");
+    });
+    $("#jquerySelectorB5").mousedown(function(){
+        $("#selector [type]").css("visibility","hidden");
+    });
+    $("#jquerySelectorB6").mousedown(function(){
+        $("#selector *").fadeToggle();
+    });
+
+    $("#jquerySelectorB7").mousedown(function(){
+        $("#selector *").fadeToggle(2000);
+    });
+
+    $("#jquerySelectorB8").mousedown(function(){
+        alert($("#selector").html());
+    });
+
+    $("#jquerySelectorB9").mousedown(function(){
+        alert($("#selector textarea").val()); 
+    });
+
+    $("#jquerySelectorB10").mousedown(function(){
+        $("#selector img").attr("src","https://images.ecestaticos.com/A_bqFc8XtsuX6QCfGI0O4fAmd10=/218x0:1920x1277/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F57d%2F362%2F826%2F57d36282607704724f677445092183d5.jpg");
+    });
+
+    $(window).mouseup(function(){
+        $("#selector *").css("visibility","visible");
+    });
     
 });
+
+
+
+
 
 
 //$("#txt").hide();
