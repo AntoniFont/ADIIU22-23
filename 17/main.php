a1945f6ecefbae19936640365e627af98e5af5d7
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2022-11-16 19:26:24 +0000

    Terminado nivel 17

--- 17/main.php
+++ 17/main.php
@@ -16,6 +16,18 @@
         if(!$conection){
             echo "<p>No se pudo conectar</p>";
         }
+        mysqli_select_db($conection,"prueba");
+        $resultado = mysqli_query($conection,"SELECT * FROM tablaprueba ORDER BY id");
+        while($fila = mysqli_fetch_array($resultado)){
+            //echo "<p>".$fila["id"]."<br>"
+            //echo print_r($fila);
+            echo "<p>";
+            for($i = 0; $i<count($fila)/2 ; $i++){
+                echo $fila[$i]." ";
+            }
+            echo "</p>";
+        }
     ?>
+
 </body>
 </html>
