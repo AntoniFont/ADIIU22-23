51387c2bc28ccd961244226756caeec05f6787e3
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2022-11-17 13:32:02 +0000

    En el nivel 17 me habia dejado la parte de ajax, ahora ya esta terminado

--- 17/backend.php
+++ 17/backend.php
@@ -1,23 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP 2</title>
-    <link rel="stylesheet" src="./style.css">
-</head>
-<body>
-    <h1>PHP 2</h1>
-    <h1>Bienvenido</h1>
-    <p>Ejercicio: Conectar con una base de datos y leer un registro</p>
-    <?php
+<?php
         $conection = mysqli_connect("localhost","root",""); 
         if(!$conection){
             echo "<p>No se pudo conectar</p>";
         }
         mysqli_select_db($conection,"prueba");
         $resultado = mysqli_query($conection,"SELECT * FROM tablaprueba ORDER BY id");
+        
+        
+        
         while($fila = mysqli_fetch_array($resultado)){
             //echo "<p>".$fila["id"]."<br>"
             //echo print_r($fila);
@@ -27,7 +17,4 @@
             }
             echo "</p>";
         }
-    ?>
-
-</body>
-</html>
+?>
