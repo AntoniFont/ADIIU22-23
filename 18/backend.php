8e751befafb8fc04cfcb0168e85b143dbb293d39
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2022-11-18 12:02:41 +0000

    ya esta acabado el nivel 18

--- 18/backend.php
+++ 18/backend.php
@@ -1,10 +1,19 @@
 <?php
-//OBTENEMOS LOS DATOS
-$input = $_POST["input"];
+//INICIAMOS LA CONEXION
 $conection = mysqli_connect("localhost", "root", "");
 if (!$conection) {
     echo "<p>No se pudo conectar</p>";
+    die("No se pudo conectar");
 }
+//OBTENEMOS LOS DATOS Y LOS TRATAMOS SI FUESE NECESARIO
+$isSeguro = $_POST["isSeguro"];
+$input = "";
+if(strcmp($isSeguro,"true") == 0){ // if($isSeguro == true)
+    $input = mysqli_real_escape_string($conection,$_POST["input"]);
+}else{
+    $input = $_POST["input"];
+}
+//ENTRAMOS EN LA BASE DE DATOS CORRECTA Y EJECUTAMOS LA QUERY
 mysqli_select_db($conection, "prueba");
 $query = "SELECT * FROM tablaprueba where id = \"".$input."\"";
 echo "<p>La query es: " . $query."</p>";
